0c836ebab717c1a3ff9f6586d255e06433881576
Andrew Grigorev <andrew@ei-grad.ru>
2017-06-07 02:56:44 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import "os"
+import "time"
+import "fmt"
+import "crypto/tls"
+
+import "github.com/dustin/go-humanize"
+
+var exitcode int
+
+func main() {
+	for _, i := range os.Args[1:] {
+		err := checkHost(i)
+		if err != nil {
+			fmt.Printf("can't check %s: %s\n", i, err)
+			panic(err)
+		}
+	}
+	os.Exit(exitcode)
+}
+
+func checkHost(host string) (err error) {
+	conn, err := tls.Dial("tcp", host, &tls.Config{
+		InsecureSkipVerify: true,
+	})
+	if err != nil {
+		return
+	}
+	conn.Close()
+	for _, cert := range conn.ConnectionState().PeerCertificates {
+		if time.Now().AddDate(0, 0, 7).After(cert.NotAfter) {
+			exitcode = 1
+			fmt.Printf("Certificate for %s (%s) expires in %s\n",
+				host, cert.Subject.CommonName,
+				humanize.Time(cert.NotAfter))
+		}
+	}
+	return
+}
